2a474660a06e96517fa269d30afa6efdf9abc544
apierro <antonio.pierro@gmail.com>
2019-08-06 18:03:47 +0200

    Added the normalize method

--- src/reducer/reducer.jsx
+++ src/reducer/reducer.jsx
@@ -3,11 +3,16 @@ import React from "react";
 class Reducer extends React.Component {
     constructor(props) {
         super(props);
-        window.props = props.cols
+        this.props = props.cols
             .filter(o => !this.arePK(o))
             .filter(o => !this.areManyCategorical(o));
+        this.normalize();
+        window.miro = this.props;
     }
-    // Check if there is a Primary Key
+    areFLoat = nums => {
+        return nums.some(num => num % 1 !== 0);
+    };
+    areManyCategorical = o => o.type === "s" && o.distinctValuesCount > 5;
     arePK = o => {
         return (
             o.type === "n" &&
@@ -15,17 +20,23 @@ class Reducer extends React.Component {
             !this.areFLoat(o.data.slice(1))
         );
     };
-
-    areFLoat = nums => {
-        return nums.some(num => num % 1 !== 0);
-    };
-
-    areManyCategorical = o => o.type === "s" && o.distinctValuesCount > 5;
-
     strings2Numbers() {
         this.props.cols.map(o =>
             o.type === "n" ? o.map((o, i) => (i > 0 ? Number(o) : o)) : o
         );
+    }
+    normalize() {
+        this.props.map(o => {
+            let result = new String(o.distinctValuesCount);
+            let replaceMap = new Map();
+            replaceMap.set(/^[1-3]$/, "1P");
+            replaceMap.set(/^[4-9]$/, "2P");
+            replaceMap.set(/^[1-9]\d{1,}$/, "3P");
+            replaceMap.forEach(function(newString, old) {
+                result = result.replace(old, newString);
+            });
+            o.normalized = result;
+        });
     }
 }
 
