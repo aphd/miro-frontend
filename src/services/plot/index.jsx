de9de49c3d9bb1461885f7f39cd70d0d3ec7d5ba
apierro <antonio.pierro@gmail.com>
2019-08-22 17:16:23 +0200

    Creating explicit public interfaces for plot module

---
+++ src/services/plot/index.jsx
@@ -0,0 +1,4 @@
+export { default as Bar } from "./bar.jsx";
+export { default as Pie } from "./pie.jsx";
+export { default as Box } from "./box.jsx";
+export { default as HeatMap } from "./heat-map";
